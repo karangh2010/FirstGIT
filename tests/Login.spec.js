a0f8c0670fa62df0aa280a427f9284271f2d5a50
karangh2010 <karanspl20@gmail.com>
2026-03-04 15:00:12 +0530

    file added

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -5,8 +5,4 @@ test('Login',async({page})=>{
     await page.locator('//input[@id="email"]').fill('karanspl20@gmail.com')
     await page.locator('//input[@name="pass"]').fill('FtyfghcvbB@1997')
     await page.locator('//button[@type="submit"]').click()
-    // await page.pause();
-
-
-
 })
